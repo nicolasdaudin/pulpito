404a393a43227e91e12736b5e1c69338e56937ab
nicdo77 <nicolas.daudin@gmail.com>
2022-09-12 22:06:59 +0200

    Added more specific test cases for validator

--- utils/validator.test.js
+++ utils/validator.test.js
@@ -2,13 +2,17 @@ const validator = require('./validator');
 
 describe('validator utils ', () => {
   describe('isCommaSeparatedAlpha', () => {
-    test('should return true when argument is a comma separated string of letters', function () {
+    test('should return true when argument is a unique string of letters', function () {
       expect(validator.isCommaSeparatedAlpha('MAD')).toBe(true);
+    });
+    test('should return true when argument is a comma separated string of letters', function () {
       expect(validator.isCommaSeparatedAlpha('MAD,OPO,BRU')).toBe(true);
     });
-    test('should return false when argument is not a comma separated string of letters', function () {
+    test('should return false when argument contains numbers', function () {
       expect(validator.isCommaSeparatedAlpha('MAD2')).toBe(false);
       expect(validator.isCommaSeparatedAlpha('MAD2,OPO,BRU')).toBe(false);
+    });
+    test('should return false when argument has not the correct comma-separator', function () {
       expect(validator.isCommaSeparatedAlpha('MAD;OPO;BRU')).toBe(false);
       expect(validator.isCommaSeparatedAlpha('MAD-OPO-BRU')).toBe(false);
       expect(validator.isCommaSeparatedAlpha('MAD OPO BRU')).toBe(false);
@@ -16,13 +20,17 @@ describe('validator utils ', () => {
   });
 
   describe('isCommaSeparatedNumeric', () => {
-    test('should return true when argument is a comma separated string of numbers', function () {
+    test('should return true when argument is a number', function () {
       expect(validator.isCommaSeparatedNumeric('1')).toBe(true);
+    });
+    test('should return true when argument is a comma separated string of numbers', function () {
       expect(validator.isCommaSeparatedNumeric('1,1,1')).toBe(true);
     });
-    test('should return false when argument is not a comma separated string of numbers', function () {
+    test('should return false when argument contains letters', function () {
       expect(validator.isCommaSeparatedNumeric('MAD')).toBe(false);
       expect(validator.isCommaSeparatedNumeric('1,2,a')).toBe(false);
+    });
+    test('should return false when argument has not the correct comma-separator', function () {
       expect(validator.isCommaSeparatedNumeric('1;2;3')).toBe(false);
       expect(validator.isCommaSeparatedNumeric('1-2-3')).toBe(false);
       expect(validator.isCommaSeparatedNumeric('1 2 3')).toBe(false);
