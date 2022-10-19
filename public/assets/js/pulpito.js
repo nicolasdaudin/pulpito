922969e759e518857d4fdb1b9b8b4634a51debcd
nicdo77 <nicolas.daudin@gmail.com>
2022-10-19 08:01:27 +0200

    Removed dependency to jquery in pulpito.js

--- public/assets/js/pulpito.js
+++ public/assets/js/pulpito.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
-  $('#addMulticityRow').on('click', function () {
+document
+  .querySelector('#addMulticityRow')
+  .addEventListener('click', function () {
     if (document.querySelectorAll('.multi_city_form').length === 9) {
       alert('Max City Limit Reached!!');
       return;
@@ -49,24 +50,11 @@ $(document).ready(function () {
 
     formWrapper.appendChild(newCityForm);
   });
-  // Remove Button Click
-  $(document).on('click', function (e) {
-    if (e.target.id === 'remove_multi_city') {
-      $(e.target).parent().closest('.multi_city_form').remove();
-    }
-  });
-});
-
-$('.btn-add,.btn-subtract').on('click touchstart', function () {
-  const qadult = $('#f-qadult').val();
-  const qchild = $('#f-qchild').val();
-  const qinfant = $('#f-qinfant').val();
-
-  $('.qstring').text(
-    ` ${qadult} Adults - ${qchild} Childs - ${qinfant} Infants`
-  );
-  event.stopPropagation();
-  event.preventDefault();
+// Remove Button Click
+document.addEventListener('click', function (e) {
+  if (e.target.id === 'remove_multi_city') {
+    e.target.parentElement.closest('.multi_city_form').remove();
+  }
 });
 
 function updateOriginPassengersCount(index) {
@@ -134,140 +122,150 @@ const substractBtnHandler = function (e) {
 };
 
 // adultes
-$('.btn-add').on('click touchstart', addBtnHandler.bind({ type: '.pcount' }));
-$('.btn-subtract').on(
-  'click touchstart',
-  substractBtnHandler.bind({ type: '.pcount', minValue: 1 })
-);
+document
+  .querySelector('.btn-add')
+  .addEventListener('click', addBtnHandler.bind({ type: '.pcount' }));
+document
+  .querySelector('.btn-subtract')
+  .addEventListener(
+    'click',
+    substractBtnHandler.bind({ type: '.pcount', minValue: 1 })
+  );
 
 // enfants
-$('.btn-add-c').on('click touchstart', addBtnHandler.bind({ type: '.ccount' }));
-$('.btn-subtract-c').on(
-  'click touchstart',
-  substractBtnHandler.bind({ type: '.ccount', minValue: 0 })
-);
+document
+  .querySelector('.btn-add-c')
+  .addEventListener('click', addBtnHandler.bind({ type: '.ccount' }));
+document
+  .querySelector('.btn-subtract-c')
+  .addEventListener(
+    'click',
+    substractBtnHandler.bind({ type: '.ccount', minValue: 0 })
+  );
 
 // bébés
-$('.btn-add-in').on(
-  'click touchstart',
-  addBtnHandler.bind({ type: '.incount' })
-);
-$('.btn-subtract-in').on(
-  'click touchstart',
-  substractBtnHandler.bind({ type: '.incount', minValue: 0 })
-);
-
-$(document).ready(function () {
-  $('.cabin-list button').click(function () {
-    event.stopPropagation();
-    event.preventDefault();
-    $('.cabin-list button.active').removeClass('active');
-    $(this).addClass('active');
-  });
-
-  // handle click on "Book all flights" button on search results - to open at once all the booking links
-  const openLinks = (links) => {
-    links.forEach((link) => {
-      window.open(link, '_blank');
-    });
-  };
-
-  const flightSearchResultWrapper = document.querySelector(
-    '.flight_search_result_wrapper'
+document
+  .querySelector('.btn-add-in')
+  .addEventListener('click', addBtnHandler.bind({ type: '.incount' }));
+document
+  .querySelector('.btn-subtract-in')
+  .addEventListener(
+    'click',
+    substractBtnHandler.bind({ type: '.incount', minValue: 0 })
   );
+
+// handle click on "Book all flights" button on search results - to open at once all the booking links
+const openLinks = (links) => {
+  links.forEach((link) => {
+    window.open(link, '_blank');
+  });
+};
+
+const flightSearchResultWrapper = document.querySelector(
+  '.flight_search_result_wrapper'
+);
+if (flightSearchResultWrapper) {
   flightSearchResultWrapper.addEventListener('click', (e) => {
     const btnBookAll = e.target.closest('.btn_book_all');
     if (!btnBookAll) return;
     openLinks(JSON.parse(btnBookAll.dataset.links));
   });
+}
 
-  // update the URL in the navigation bar with info from the search query (since we can't redirect from pug)
+// update the URL in the navigation bar with info from the search query (since we can't redirect from pug)
 
-  const originInputs = document.querySelectorAll(
-    `.multi_city_form input[name='origins[][flyFrom]'`
-  );
-  if (originInputs[0].value) {
-    // form has been filled with values
-    const searchForm = document
-      .querySelector('.multi_city_form')
-      .closest('form');
+const originInputs = document.querySelectorAll(
+  `.multi_city_form input[name='origins[][flyFrom]'`
+);
+if (originInputs && originInputs[0] && originInputs[0].value) {
+  // form has been filled with values
+  const searchForm = document.querySelector('.multi_city_form').closest('form');
 
-    const formData = new FormData(searchForm);
-    const urlQueryParamsFromFormData = new URLSearchParams(formData);
+  const formData = new FormData(searchForm);
+  const urlQueryParamsFromFormData = new URLSearchParams(formData);
 
-    const newUrl = '/common?' + urlQueryParamsFromFormData.toString();
+  const newUrl = '/common?' + urlQueryParamsFromFormData.toString();
 
-    window.history.replaceState({}, document.title, newUrl);
-  }
+  window.history.replaceState({}, document.title, newUrl);
+}
 
-  // update price ranger filters on document load
-  const priceSlider = document.querySelector('#price-slider');
-  if (priceSlider) {
-    // getting options from datasets-
-    const { minPossiblePrice, maxPossiblePrice, priceFrom, priceTo } =
-      priceSlider.dataset;
+// update price ranger filters on document load
+const priceSlider = document.querySelector('#price-slider');
+if (priceSlider) {
+  // getting options from datasets-
+  const { minPossiblePrice, maxPossiblePrice, priceFrom, priceTo } =
+    priceSlider.dataset;
 
-    // remove the slider created by the template, not sure how it works or how to access it.
-    // and we don't want to modify the template...
+  // remove the slider created by the template, not sure how it works or how to access it.
+  // and we don't want to modify the template...
 
-    priceSlider.noUiSlider.destroy();
-    noUiSlider.create(priceSlider, {
-      start: [+priceFrom, +priceTo],
-      connect: true,
-      step: 25,
-      margin: 50,
-      range: {
-        min: +minPossiblePrice,
-        max: +maxPossiblePrice,
-      },
-      tooltips: true,
-      format: wNumb({
-        decimals: 0,
-        suffix: ' €',
-      }),
-    });
-  }
+  priceSlider.noUiSlider.destroy();
+  noUiSlider.create(priceSlider, {
+    start: [+priceFrom, +priceTo],
+    connect: true,
+    step: 25,
+    margin: 50,
+    range: {
+      min: +minPossiblePrice,
+      max: +maxPossiblePrice,
+    },
+    tooltips: true,
+    format: wNumb({
+      decimals: 0,
+      suffix: ' €',
+    }),
+  });
+}
 
-  const btnRadioMaxConnections = document.querySelector(
-    '.btn_radio_max_connections'
-  );
+const btnRadioMaxConnections = document.querySelector(
+  '.btn_radio_max_connections'
+);
 
-  // handle click on 'Apply' for search filters
-  const btnApplySearchFilters = document.querySelector('.apply_search_filters');
-  if (btnApplySearchFilters) {
-    btnApplySearchFilters.addEventListener('click', (e) => {
-      let urlParams = [];
-      if (priceSlider) {
-        // get sliders value
-        const [priceFrom, priceTo] = priceSlider.noUiSlider
-          .get()
-          .map((value) => Number(value.replace(' €', '')));
+// handle click on 'Apply' for search filters
+const btnApplySearchFilters = document.querySelector('.apply_search_filters');
+if (btnApplySearchFilters) {
+  btnApplySearchFilters.addEventListener('click', (e) => {
+    let urlParams = [];
 
-        urlParams.push(`&priceFrom=${priceFrom}`, `&priceTo=${priceTo}`);
-      }
+    // document.querySelector('.preloader').style.display = 'block';
 
-      if (btnRadioMaxConnections) {
-        const btnRadioMaxConnectionsChecked =
-          btnRadioMaxConnections.querySelector(
-            'input[name="maxConnections"]:checked'
-          );
-        urlParams.push(
-          `&maxConnections=${btnRadioMaxConnectionsChecked.value}`
+    if (priceSlider) {
+      // get sliders value
+      const [priceFrom, priceTo] = priceSlider.noUiSlider
+        .get()
+        .map((value) => Number(value.replace(' €', '')));
+
+      urlParams.push(`&priceFrom=${priceFrom}`, `&priceTo=${priceTo}`);
+    }
+
+    if (btnRadioMaxConnections) {
+      const btnRadioMaxConnectionsChecked =
+        btnRadioMaxConnections.querySelector(
+          'input[name="maxConnections"]:checked'
         );
-      }
+      urlParams.push(`&maxConnections=${btnRadioMaxConnectionsChecked.value}`);
+    }
 
-      // retrive sort paran
-      const sortResultsElement = document.querySelector('.sort_results');
-      if (sortResultsElement) {
-        urlParams.push(`&sort=${sortResultsElement.dataset.sort}`);
-      }
+    // retrive sort paran
+    const sortResultsElement = document.querySelector('.sort_results');
+    if (sortResultsElement) {
+      urlParams.push(`&sort=${sortResultsElement.dataset.sort}`);
+    }
 
-      // get current url and append these two new params
-      const currentUrl = window.location.href;
-      const newUrl = `${currentUrl}${urlParams.join('')}`;
+    // get current url and append these two new params
+    const currentUrl = window.location.href;
+    const newUrl = `${currentUrl}${urlParams.join('')}`;
 
-      // open location
-      window.open(newUrl, '_self');
-    });
-  }
+    // open location
+    window.open(newUrl, '_self');
+  });
+}
+
+/**
+ * everytime the window is about to be refreshed (form submit, or click on a previous or next link, or on a sort filter...), we show the preloader.
+ */
+window.addEventListener('beforeunload', (e) => {
+  const preloader = document.querySelector('.preloader');
+  if (!preloader) return;
+  preloader.style.display = 'block';
 });
